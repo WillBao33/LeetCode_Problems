cc19d7f192b7bc4100f84d94e761ce0343586226
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-01-17 22:24:42 -0500

    Update 9_palindrome_number.py

--- 9_palindrome_number.py
+++ 9_palindrome_number.py
@@ -12,3 +12,14 @@ class Solution(object):
             return True
         else:
             return False
+
+# below is the way without converting to string
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return 0
+
+        reverse = 0 
+        while x > reverse:
+            reverse = reverse * 10 + x % 10 
+            x = x // 10
+
+        return x == reverse or x == reverse // 10
