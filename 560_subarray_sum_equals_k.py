d25e890f6677d19da15d641becab6e096e49aec1
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-01 13:43:16 -0500

    Create 560_subarray_sum_equals_k.py

---
+++ 560_subarray_sum_equals_k.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        counts = defaultdict(int)
+        counts[0] = 1
+        ans = curr = 0
+
+        for num in nums:
+            curr += num
+            ans += counts[curr - k]
+            counts[curr] += 1
+
+        return ans
+
