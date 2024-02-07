8bc272b8d74b111da0d0eed3f3ed474152d7e806
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-07 18:36:22 -0500

    Create 232_implement_queue_using_stacks.py

---
+++ 232_implement_queue_using_stacks.py
@@ -0,0 +1,29 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return True if not self.stack else False
