d226f72da73af770acd3694134653c7487154469
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-14 22:28:08 -0500

    Create 701_insert_into_a_binary_search_tree.py

---
+++ 701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+
+        return root
