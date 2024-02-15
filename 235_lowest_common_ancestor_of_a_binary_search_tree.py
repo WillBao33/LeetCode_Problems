6a26137aa35b40b11fe7abb6cadbfb0cd1e3c378
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-15 00:27:58 -0500

    Create 235_lowest_common_ancestor_of_a_binary_search_tree.py

---
+++ 235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        node = root
+
+        while node:
+            if p.val > node.val and q.val > node.val:
+                node = node.right
+            elif p.val < node.val and q.val < node.val:
+                node = node.left
+            else:
+                return node
+
+        return None
