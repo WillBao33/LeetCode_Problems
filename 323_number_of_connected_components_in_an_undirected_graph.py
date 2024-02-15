6ea57ef78ec6fb71cbde96c03acd177461e00e71
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-15 16:08:56 -0500

    Create 323_number_of_connected_components_in_an_undirected_graph.py

---
+++ 323_number_of_connected_components_in_an_undirected_graph.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        def dfs(node):
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+    
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        seen = set()
+        ans = 0
+        for i in range(n):
+            if i not in seen:
+                ans += 1
+                seen.add(i)
+                dfs(i)
+
+        return ans
