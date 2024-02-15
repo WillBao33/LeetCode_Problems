04c43970c1b318c1037b72c0a4d6444086d00232
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-15 15:06:53 -0500

    Create 841_keys_and_rooms.py

---
+++ 841_keys_and_rooms.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        def dfs(node):
+            for neighbor in rooms[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
+        seen = {0}
+        dfs(0)
+        return len(seen) == len(rooms)
