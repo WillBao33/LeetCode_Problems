cfe50a8df83e650ec8751a6804fbe272a9be9aae
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-20 13:35:37 -0500

    Create 1046_last_stone_weight.py

---
+++ 1046_last_stone_weight.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+            if first != second:
+                heapq.heappush(stones,-abs(first - second))
+
+        return -stones[0] if stones else 0
