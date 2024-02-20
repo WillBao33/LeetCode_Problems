c2f65f0e267fe902a012320acea8b4e98b4dc672
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-20 15:35:16 -0500

    Create 703_kth_largest_element_in_a_stream.py

---
+++ 703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,25 @@
+import heapq
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.heap = nums[:k]
+        heap = heapq.heapify(self.heap)
+        for num in nums[k:]:
+            heapq.heappushpop(self.heap,num)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        else:
+            heapq.heappushpop(self.heap, val)
+
+        return self.heap[0]
