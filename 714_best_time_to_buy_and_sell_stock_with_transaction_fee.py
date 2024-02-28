94555a25040412f731488a8435c8f4ba3ce70ef0
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-02-28 14:17:35 -0500

    Create 714_best_time_to_buy_and_sell_stock_with_transaction_fee.py

---
+++ 714_best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        @cache
+        def dp(i, holding):
+            if i == len(prices):
+                return 0
+
+            ans = dp(i + 1, holding)
+            if holding:
+                ans = max(ans, prices[i] + dp(i + 1, False) - fee)
+            else:
+                ans = max(ans, -prices[i] + dp(i + 1, True))
+
+            return ans
+
+        return dp(0, False)
