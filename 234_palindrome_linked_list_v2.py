2d882421b41d89eb43175b117b7ed94d7b1113bb
WillBao33 <81249924+WillBao33@users.noreply.github.com>
2024-03-31 18:30:37 -0400

    Create 234_palindrome_linked_list_v2.py

---
+++ 234_palindrome_linked_list_v2.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        first = head
+        arr = []
+        while first is not None:
+            arr.append(first.val)
+            first = first.next
+
+        return arr == arr[::-1]
